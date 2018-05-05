c51dccd4d20973f01ac5ba33262af8903d131c5c
James Bowman <jamesb@excamera.com>
2018-05-04 17:06:12 -0700

    LED string driver

---
+++ python/samples/leds.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import colorsys
+
+from spidriver import SPIDriver
+
+if __name__ == '__main__':
+    s = SPIDriver()
+    L = 110
+
+    blanking = [0] * ((L + 31) // 32)
+    d = [0x80,0x80,0x80] * L
+    s.write(blanking + d + blanking)
+
+    rainbow = sum([colorsys.hsv_to_rgb(float(i) / L, 1, 1) for i in range(L)], ())
+    rainbow = [0x80 + int(127 * v) for v in rainbow]
+    while True:
+        s.write(rainbow + blanking)
+        rainbow = rainbow[3:] + rainbow[:3]

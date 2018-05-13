b8e361b9308ce9f4e8fe634293e1d9e6aef74a49
James Bowman <jamesb@excamera.com>
2018-05-12 19:19:03 -0700

    Update leds.py

--- python/samples/leds.py
+++ python/samples/leds.py
@@ -9,11 +9,10 @@ if __name__ == '__main__':
     L = 300
 
     blanking = [0] * ((L + 31) // 32)
-    d = [0x80,0x80,0x80] * L
-    s.write(blanking + d + blanking)
+    s.write(blanking + [0x80,0x80,0x80] * L + blanking)
 
     rainbow = sum([colorsys.hsv_to_rgb(float(i) / L, 1, 1) for i in range(L)], ())
-    rainbow = [0x80 + int(127 * v) for v in rainbow]
+    rainbow = [int(128 + 127 * v) for v in rainbow]
     while True:
         s.write(rainbow + blanking)
         rainbow = rainbow[3:] + rainbow[:3]

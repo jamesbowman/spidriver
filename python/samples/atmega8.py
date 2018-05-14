742817e51891d274d68815b6acd3ee4865b1a02a
James Bowman <jamesb@excamera.com>
2018-05-13 17:56:43 -0700

    Full Atmega programmer

--- python/samples/atmega8.py
+++ python/samples/atmega8.py
@@ -1,69 +1,117 @@
 #!/usr/bin/env python3
 
+# Arduino programmer for SPIDriver
+#
+# Connect to the ISP header like this:
+#
+#                +------+
+#  (yellow) MISO | 1  2 | 3.3V (red)
+#  (orange)  SCK | 3  4 | MOSI (green)
+#  (purple)    A | 5  6 | GND  (black)
+#                +------+
+#
+# Then load a hex file with:
+#
+#   ./atmega8.py foo.hex
+#
+# See Atmega328 datasheet section 31.8 for protocol details.
+#
+
 import sys
 import time
+import array
 
 from spidriver import SPIDriver
 
 class ConnectionError(Exception):
     pass
 
-def pulse_reset(s):
-    # +ve pulse on RESET
-    s.seta(1)
-    s.seta(0)
+class AtMega:
+    def __init__(self, s):
+        self.s = s
 
-def programming_enable(s):
-    for i in range(10):
-        pulse_reset(s)
-        response = s.writeread([0xac, 0x53, 0, 0])
-        print(response)
-        if response[2] == 0x53:
+    def pulse_reset(self):
+        # +ve pulse on RESET
+        self.s.seta(1)
+        self.s.seta(0)
+
+    def programming_enable(self):
+        # Datasheet recommended method for getting into programming sync
+        for i in range(10):
+            self.pulse_reset()
+            response = self.s.writeread(bytes([0xac, 0x53, 0, 0]))
+            if response[2] == 0x53:
+                return
+            print(repr(response))
+        raise ConnectionError
+
+    def multifetch(self, insns):
+        # Send insns, return last byte of each packet
+        r = self.s.writeread(bytes(insns))
+        return [r[i] for i in range(3, len(insns), 4)]
+        
+    def lohi(self, bb):
+        (lo, hi) = self.multifetch(bb)
+        return lo | (hi << 8)
+
+    def read_program_memory(self, a):
+        return self.lohi([0x20, a >> 8, a & 0xff, 0,
+                          0x28, a >> 8, a & 0xff, 0])
+
+    def read_fuses(self):
+        return self.lohi([0x50, 0x00, 0, 0,
+                          0x58, 0x08, 0, 0])
+
+    def waitready(self):
+        # Poll RDY/BSY until operation completes
+        while True:
+           (rdy,) = self.multifetch([0xf0, 0, 0, 0])
+           if (rdy & 1) == 0:
             return
-    raise ConnectionError
 
-def multifetch(s, insns):
-    r = s.writeread(insns)
-    return [r[i] for i in range(3, len(insns), 4)]
-    
-def lohi(s, bb):
-    (lo, hi) = multifetch(s, bb)
-    return lo | (hi << 8)
+    def erase(self):
+        self.s.write(bytes([0xac, 0x80, 0, 0]))
+        self.waitready()
 
-def read_program_memory(s, a):
-    return lohi(s, [0x20, a >> 8, a & 0xff, 0,
-                    0x28, a >> 8, a & 0xff, 0])
+    def write_page(self, a, page):
+        for i,v in enumerate(page):
+            self.s.write(bytes([0x40, 0, i, v & 0xff]))
+            self.s.write(bytes([0x48, 0, i, v >> 8]))
+        self.s.write(bytes([0x4c, a >> 8, a & 0xff, 0]))
+        self.waitready()
 
-def read_fuses(s):
-    return lohi(s, [0x50, 0x00, 0, 0,
-                    0x58, 0x08, 0, 0])
+    def load(self, program):
+        for i in range(0, len(program), 64):
+            self.write_page(i, program[i:i+64])
 
-def erase(s):
-    s.write([0xac, 0x80, 0, 0])
-    time.sleep(.0105)
+    def verify(self, program):
+        for i,p in enumerate(program):
+            assert self.read_program_memory(i) == p, ("Mismatch at offset %d" % i)
 
-def write_page(s, a, page):
-    for i,v in enumerate(page):
-        s.write([0x40, 0, i, v & 0xff])
-        s.write([0x48, 0, i, v >> 8])
-    s.write([0x4c, a >> 8, a & 0xff, 0])
-    time.sleep(.1)
+    def release(self):
+        self.s.seta(1)
+        self.s.detach()
 
 def read_hex(filename):
+    m = []
+    def hexbyte(l, n):
+        return int(l[2*n+1:2*n+3], 16)
+    def hexword(l, n):
+        return hexbyte(l, n) | (hexbyte(l, n + 1) << 8)
     for l in open(filename, "rt"):
-        print(l)
+        count = hexbyte(l, 0)
+        for i in range(4, 4 + count, 2):
+            m.append(hexword(l, i))
+    # print("".join(["%04x " % w for w in m]))
+    return m
 
 if __name__ == '__main__':
-    program = read_hex(sys.argv[1])
-
-    s = SPIDriver()
-    programming_enable(s)
-    erase(s)
-    write_page(s, 64, range(128))
-    s.detach()
-    """
-    print("Fuses: %04x" % read_fuses(s))
-    # Fuses d9ff
-    for i in range(140):
-        print("%04x: %04x" % (i, read_program_memory(s, i)))
-    """
+    program = read_hex(sys.argv[2])
+    s = SPIDriver(sys.argv[1])
+    a = AtMega(s)
+    a.programming_enable()
+    a.erase()
+    a.load(program)
+    a.verify(program)
+    a.release()
+    print("Loaded and verified program %s" % sys.argv[2])

38b510e7e4d30a3cb867f3db5aa52525d498a7fe
James Bowman <jamesb@excamera.com>
2018-08-02 15:15:56 -0700

    All widgets functional

--- python/samples/wxgui.py
+++ python/samples/wxgui.py
@@ -1,5 +1,6 @@
 import wx
 import time
+import struct
 import threading
 from functools import partial
 
@@ -14,15 +15,41 @@ def ping_thr(win):
         time.sleep(1)
         wx.PostEvent(win, MooEvent())
 
+class HexTextCtrl(wx.TextCtrl):
+    def __init__(self, *args, **kwargs):
+        super(HexTextCtrl, self).__init__(*args, **kwargs)
+        self.Bind(wx.EVT_TEXT, self.on_text)
+    def on_text(self, event):
+        event.Skip()
+        selection = self.GetSelection()
+        value = self.GetValue().upper()
+        hex = "0123456789ABCDEF"
+        value = "".join([c for c in value if c in hex])
+        self.ChangeValue(value)
+        self.SetSelection(*selection)
+
 class Frame(wx.Frame):
     def __init__(self):
 
         self.sd = SPIDriver("/dev/ttyUSB0")
 
+        def widepair(a, b):
+            r = wx.BoxSizer(wx.HORIZONTAL)
+            r.Add(a, 1, wx.LEFT)
+            r.AddStretchSpacer(prop=1)
+            r.Add(b, 1, wx.RIGHT)
+            return r
+
         def pair(a, b):
             r = wx.BoxSizer(wx.HORIZONTAL)
-            r.Add(a, 1, wx.LEFT, border = 1)
-            r.Add(b, 0, wx.RIGHT, border = 1)
+            r.Add(a, 1, wx.LEFT)
+            r.Add(b, 0, wx.RIGHT)
+            return r
+
+        def rpair(a, b):
+            r = wx.BoxSizer(wx.HORIZONTAL)
+            r.Add(a, 0, wx.LEFT)
+            r.Add(b, 1, wx.RIGHT)
             return r
 
         def label(s):
@@ -30,7 +57,14 @@ class Frame(wx.Frame):
 
         def hbox(items):
             r = wx.BoxSizer(wx.HORIZONTAL)
-            [r.Add(i) for i in items]
+            [r.Add(i, 0, wx.EXPAND) for i in items]
+            return r
+
+        def hcenter(i):
+            r = wx.BoxSizer(wx.HORIZONTAL)
+            r.AddStretchSpacer(prop=1)
+            r.Add(i, 3, wx.CENTER)
+            r.AddStretchSpacer(prop=1)
             return r
 
         def vbox(items):
@@ -40,10 +74,11 @@ class Frame(wx.Frame):
 
         wx.Frame.__init__(self, None, -1, "SPIDriver")
 
-        self.label_voltage = wx.StaticText(self, label = "-")
-        self.label_current = wx.StaticText(self, label = "-")
-        self.label_temp = wx.StaticText(self, label = "-")
-        self.label_uptime = wx.StaticText(self, label = "-")
+        self.label_serial = wx.StaticText(self, label = "-", style = wx.ALIGN_RIGHT)
+        self.label_voltage = wx.StaticText(self, label = "-", style = wx.ALIGN_RIGHT)
+        self.label_current = wx.StaticText(self, label = "-", style = wx.ALIGN_RIGHT)
+        self.label_temp = wx.StaticText(self, label = "-", style = wx.ALIGN_RIGHT)
+        self.label_uptime = wx.StaticText(self, label = "-", style = wx.ALIGN_RIGHT)
 
         self.refresh()
 
@@ -59,12 +94,35 @@ class Frame(wx.Frame):
         ckA.Bind(wx.EVT_CHECKBOX, self.check_a)
         ckB.Bind(wx.EVT_CHECKBOX, self.check_b)
 
+        self.txMISO = wx.TextCtrl(self, style=wx.TE_READONLY | wx.TE_RIGHT | wx.TE_DONTWRAP)
+        self.txMOSI = wx.TextCtrl(self, style=wx.TE_READONLY | wx.TE_RIGHT | wx.TE_DONTWRAP)
+        self.txMISO.SetBackgroundColour(wx.Colour(224, 224, 224))
+        self.txMOSI.SetBackgroundColour(wx.Colour(224, 224, 224))
+
+
+        self.txVal = HexTextCtrl(self, size=wx.DefaultSize, style=0)
+        self.txVal.SetMaxLength(2)
+        self.txVal.SetFont(wx.Font(14,
+                              wx.FONTFAMILY_TELETYPE,
+                              wx.FONTSTYLE_NORMAL,
+                              wx.FONTWEIGHT_BOLD))
+        txButton = wx.Button(self, label = "Transfer")
+        txButton.Bind(wx.EVT_BUTTON, partial(self.transfer, self.txVal))
+
         self.SetSizerAndFit(vbox([
-            pair(label("Voltage"), self.label_voltage),
-            pair(label("Current"), self.label_current),
-            pair(label("Temp."), self.label_temp),
-            pair(label("Running"), self.label_uptime),
-            hbox([ckCS, ckA, ckB]),
+            hcenter(vbox([
+                widepair(label("Serial"), self.label_serial),
+                widepair(label("Voltage"), self.label_voltage),
+                widepair(label("Current"), self.label_current),
+                widepair(label("Temp."), self.label_temp),
+                widepair(label("Running"), self.label_uptime),
+            ])),
+            label(""),
+            rpair(label("MISO"), self.txMISO),
+            rpair(label("MOSI"), self.txMOSI),
+            label(""),
+            hcenter(pair(ckCS, hbox([ckA, ckB]))),
+            hcenter(pair(self.txVal, txButton)),
             ]))
         self.SetAutoLayout(True)
 
@@ -77,6 +135,7 @@ class Frame(wx.Frame):
 
     def refresh(self):
         self.sd.getstatus()
+        self.label_serial.SetLabel(self.sd.serial)
         self.label_voltage.SetLabel("%.2f V" % self.sd.voltage)
         self.label_current.SetLabel("%d mA" % self.sd.current)
         self.label_temp.SetLabel("%.1f C" % self.sd.temp)
@@ -94,7 +153,15 @@ class Frame(wx.Frame):
     def check_b(self, e):
         self.sd.setb(e.EventObject.GetValue())
 
-app = wx.App(0)
-f = Frame()
-f.Show(True)
-app.MainLoop()
+    def transfer(self, htc, e):
+        txb = int(htc.GetValue(), 16)
+        rxb = struct.unpack("B", self.sd.writeread(struct.pack("B", txb)))[0]
+        self.txMOSI.AppendText(" %02x" % txb)
+        self.txMISO.AppendText(" %02x" % rxb)
+        htc.ChangeValue("")
+
+if __name__ == '__main__':
+    app = wx.App(0)
+    f = Frame()
+    f.Show(True)
+    app.MainLoop()
